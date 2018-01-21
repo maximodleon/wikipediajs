85f74d4e746e21896ca889ed210539d791cbffa0
maximo <maximodleon@gmail.com>
2018-01-21 09:15:15 -0400

    feat(featuredfeed): Add featured feed to api
    Add featured feed to api available endpoints

--- src/wiki.js
+++ src/wiki.js
@@ -1,7 +1,6 @@
-const {
-  doSearch,
-  doRandom
-} = require('./generators/index')
+const { doSearch, doRandom } = require('./generators/index')
+const { queryApi } = require('./utils/urlUtils')
+const  convert = require('xml-js')
 
 function search(searchTerm, lang = "en", options) {
   return doSearch(searchTerm, lang, options)
@@ -11,7 +10,25 @@ function random(lang = "en", options) {
   return doRandom(lang, options)
 }
 
+function featured(options = { feed: "potd"}) {
+  const params = Object.assign({}, options, {
+    action: 'featuredfeed'
+  })
+
+  return queryApi("en", params)
+    .then((response) => { 
+      if (response){
+        const json = convert.xml2js(response, {compact: false}) 
+        if (json.elements && json.elements.length) {
+          return json.elements[0]
+        }
+      }
+    }
+   )
+}
+
 module.exports = {
   search,
-  random
+  random,
+  featured
 };
